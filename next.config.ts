93edf5ed4732c0e6c9227ac3c6e402eab692f23e
Nabil <nabileldruzo@gmail.com>
2026-05-27 10:31:22 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.GITHUB_ACTIONS ? "/taly-v2" : "",
+  basePath: "/taly-v2",
   images: {
     unoptimized: true,
   },
